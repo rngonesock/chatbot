2945c02e8270c3910a484279cb37b90926171fe1
rudra_gadila <rudragadila@gmail.com>
2025-01-11 19:51:30 -0500

    ChatBot program using python

---
+++ chatbot.py
@@ -0,0 +1,14 @@
+#A program to create a ChatBot
+print("Welcome to ChatBot")
+print("Hi, my name is Rudra's ChatBot, what is your name?")
+name = input()
+print("Nice to meet you", name)
+print("How is your day?")
+feeling = input()
+if feeling == "Good" or feeling == "good" or feeling == "great":
+    print("Thats nice to hear")
+else: 
+    print("Hope your day gets better")
+print("What is your favorite hobby?")
+hobby = input()
+print("Nice, thats an interesting hobby")
