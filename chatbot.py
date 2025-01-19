3891703a8a91936e9764f5c6e262604de4bfa91f
rudra_gadila <rudragadila@gmail.com>
2025-01-19 16:39:33 -0500

    updated version of chatbot

--- chatbot.py
+++ chatbot.py
@@ -12,4 +12,27 @@ else:
 print("What is your favorite hobby?")
 hobby = input()
 print("Nice, thats an interesting hobby")
-print("I'll come back in one minute")
+sport = input()
+print("Do You Play Any Sports")
+print("Nice")
+print("Do you support any NFL Teams?")
+team = input()
+if team == "Ravens" or team == "ravens" or team == "Baltimore Ravens" or team == "baltimore ravens" or team == "baltimore" or team == "Baltimore":
+ print("Same!")
+else: 
+    print("Aww, boo!")
+print("Do you like any musicians")
+muchoice = input()
+if muchoice == "Yes" or muchoice == "yes" or muchoice == "yea":
+ print("Nice, Which one?")
+ artist = input()
+else:
+     print("Oh")
+print("What is your favorite movie")
+movchoice = input()
+print("Nice! Thats a really great movie.")
+print("Where are you from")
+ethnicity = input()
+print("Nice")
+print("Do you watch any shows?")
+show = input()
